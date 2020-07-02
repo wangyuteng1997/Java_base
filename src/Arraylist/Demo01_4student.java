065595fd7158f73e2742d931baf071c551aa59e3
wangyuteng <wangyuteng1997@outlook.com>
2020-07-02 23:32:16 +0800

    2

---
+++ src/Arraylist/Demo01_4student.java
@@ -0,0 +1,24 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class Demo01_4student {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        Student one = new Student("A",10);
+//        z这里因为Student类中的name和age都是private类型，赋值只能通过初始化和setter的形式赋值
+        Student two = new Student("B",10);
+        Student three = new Student("C",10);
+        Student four = new Student("D",10);
+
+        list.add(one);
+        list.add(two);
+        list.add(three);
+        list.add(four);
+
+        for (int i = 0; i < list.size(); i++) {
+            Student student = list.get(i);
+            System.out.println("姓名 ： =" + student.getName() + "," + "年龄 ：=" + student.getAge() );
+        }
+    }
+}
